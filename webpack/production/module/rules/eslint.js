8cccc1a0d746e81a7b90106ffd1ad144fb9019d2
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-08-01 11:25:32 +0200

    Add support for production webpack

--- webpack/production/module/rules/eslint.js
+++ webpack/production/module/rules/eslint.js
@@ -1,10 +1,9 @@
 module.exports.default = rules => {
-	rules.find(({use}) => use[0].loader.includes('eslint-loader'))
-		.use[0]
-		.options = {
-			failOnWarning: true,
-			failOnError: true
-		}
+	const rule = rules.find(({ loader = '' }) => loader.includes('eslint-loader'))
+	rule.options = {
+		...rule.options,
+		failOnWarning: true,
+		failOnError: true
+	}
 	return rules
 }
-
--- webpack/production/module/rules/scss.js
+++ webpack/production/module/rules/scss.js
@@ -1,12 +1,14 @@
 const loaders = ['css-loader', 'sass-loader', 'postcss-loader']
 
 module.exports.default = rules => {
-	rules.forEach(({use}) =>
-		use.forEach((u, k) => {
-			if (loaders.includes(u[k].loader)) {
-				u[k].options.sourceMaps = false
-			}
+	rules.forEach(({ use }) => {
+		Array.isArray(use) && use.forEach(u => {
+			loaders.forEach(l => {
+				if (u.loader.includes(l)) {
+					u.options.sourceMaps = false
+				}
+			})
 		})
-	)
+	})
 	return rules
 }
