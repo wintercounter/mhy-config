21e4400cf423d0de57cd231a49474b3828a7d3ef
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-08-06 11:31:14 +0200

    Update loader paths

--- webpack/root/module/rules/5-image.js
+++ webpack/root/module/rules/5-image.js
@@ -3,7 +3,14 @@ module.exports.default = rules => [
 	{
 		test: /\.(gif|png|jpe?g)$/i,
 		loaders: [
-			'file-loader?hash=sha512&digest=hex&name=[name].[hash].[ext]',
+			{
+				loader: require.resolve('file-loader'),
+				options: {
+					hash: 'sha512',
+					digest: 'hex',
+					name: '[name].[hash].[ext]',
+				}
+			},
 			{
 				loader: require.resolve('image-webpack-loader'),
 				options: {
