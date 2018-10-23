5569ea4103f4a061758cad212e3bb81b58b46b32
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-10-23 17:37:07 +0200

    feat(manifest): add support for pwa manifests

--- webpack/root/plugins.js
+++ webpack/root/plugins.js
@@ -1,9 +1,24 @@
-const ManifestPlugin = require('webpack-manifest-plugin')
+const fs = require('fs')
+const path = require('path')
+
+const WebpackManifestPlugin = require('webpack-manifest-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+
+const manifest = require('../../manifest')
 
 module.exports.default = (plugins = []) => {
+    manifest.icons.map(icon => {
+        const inCwdPath = path.resolve(process.cwd(), icon.src)
+        if (fs.existsSync(inCwdPath)) {
+            icon.src = inCwdPath
+        }
+        return icon
+    })
+
 	return plugins.concat([
-		new ManifestPlugin({
-			fileName: './manifest.json',
+        new WebpackPwaManifestPlugin(manifest),
+		new WebpackManifestPlugin({
+			fileName: './manifest.webpack.json',
 		})
 	])
 }
