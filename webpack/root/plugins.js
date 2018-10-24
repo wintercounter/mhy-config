d005815326fc858e9754c5b95ca6b0da63876e9c
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-10-24 14:51:19 +0200

    feat(webpack): add workbox support

--- webpack/root/plugins.js
+++ webpack/root/plugins.js
@@ -1,10 +1,18 @@
+import { moduleHome } from "../../index";
+
 const fs = require('fs')
 const path = require('path')
 
 const WebpackManifestPlugin = require('webpack-manifest-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
+
+const { moduleHome } = require('../../')
 
 const manifest = require('../../manifest')
+const defaultSwPath = path.resolve(moduleHome, '../../../../', 'src/sw.js')
+const projectSwPath = path.resolve(process.cwd(), 'src/sw.js')
+const swSrc = fs.existsSync(projectSwPath) ? projectSwPath : defaultSwPath
 
 module.exports.default = (plugins = []) => {
     manifest.icons.map(icon => {
@@ -16,6 +24,8 @@ module.exports.default = (plugins = []) => {
     })
 
 	return plugins.concat([
+	    new GenerateSW(),
+        new InjectManifest({ swSrc }),
         new WebpackPwaManifestPlugin(manifest),
 		new WebpackManifestPlugin({
 			fileName: './manifest.webpack.json',
