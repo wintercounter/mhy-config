1b33f5c89868f37e1acfb3d95bf8511e5e6cd110
pablo.silva <pablo.silva@doclerholding.com>
2018-10-30 07:28:05 +0100

    feat(typescript): added extra configurations on typescript config

--- typescript/index.js
+++ typescript/index.js
@@ -4,18 +4,15 @@ import { load } from '..'
 
 module.exports = module.exports.default = load('typescript', {
 	compilerOptions: {
-		// target: 'esnext',
 		moduleResolution: 'node',
 		allowJs: true,
 		noEmit: true,
 		strict: true,
 		esModuleInterop: true,
-		// module: 'commonjs',
-		// jsx: 'react'
-		// outDir: path.resolve(process.cwd(), 'dist'),
+		outDir: path.resolve(process.cwd(), 'dist'),
 		sourceMap: true,
 		noImplicitAny: true,
-		// module: 'commonjs',
+		module: 'commonjs',
 		target: 'es5',
 		jsx: 'react'
 	},
