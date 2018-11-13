df9b79395b49db841c664cdaccf45d2e269bb497
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-11-13 15:48:30 +0100

    fix(webpack): url-loader rules

--- webpack/root/module/rules/30-fonts.js
+++ webpack/root/module/rules/30-fonts.js
@@ -2,26 +2,57 @@ module.exports.default = rules => [
     ...rules,
     {
         test: /\.svg$/,
-        use: 'url-loader?limit=65000&mimetype=image/svg+xml&name=[name].[ext]'
+        use: {
+            loader: require.resolve('url-loader'),
+            options: {
+                limit: 65000,
+                mimetype: 'image/svg+xml',
+                name: '[name].[ext]'
+            }
+        }
     },
     {
         test: /\.woff$/,
-        use:
-            'url-loader?limit=65000&mimetype=application/font-woff&name=[name].[ext]'
+        use: {
+            loader: require.resolve('url-loader'),
+            options: {
+                limit: 65000,
+                mimetype: 'application/font-woff',
+                name: '[name].[ext]'
+            }
+        }
     },
     {
         test: /\.woff2$/,
-        use:
-            'url-loader?limit=65000&mimetype=application/font-woff2&name=[name].[ext]'
+        use: {
+            loader: require.resolve('url-loader'),
+            options: {
+                limit: 65000,
+                mimetype: 'application/font-woff2',
+                name: '[name].[ext]'
+            }
+        }
     },
     {
         test: /\.[ot]tf$/,
-        use:
-            'url-loader?limit=65000&mimetype=application/octet-stream&name=[name].[ext]'
+        use: {
+            loader: require.resolve('url-loader'),
+            options: {
+                limit: 65000,
+                mimetype: 'application/octet-stream',
+                name: '[name].[ext]'
+            }
+        }
     },
     {
         test: /\.eot$/,
-        use:
-            'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=[name].[ext]'
+        use: {
+            loader: require.resolve('url-loader'),
+            options: {
+                limit: 65000,
+                mimetype: 'application/vnd.ms-fontobject',
+                name: '[name].[ext]'
+            }
+        }
     }
 ]
