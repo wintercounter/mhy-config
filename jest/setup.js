ec395159afa7c1376659091cf6d23c4c2d53e7d7
Viktor Vincze <viktor.vincze@doclerholding.com>
2018-11-14 13:59:20 +0100

    feat(jest): jest-styled-components

--- jest/setup.js
+++ jest/setup.js
@@ -1,6 +1,7 @@
 import Enzyme, { shallow, render, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import 'jest-canvas-mock'
+import 'jest-styled-components'
 import fetch from 'jest-fetch-mock'
 
 Enzyme.configure({ adapter: new Adapter() })
