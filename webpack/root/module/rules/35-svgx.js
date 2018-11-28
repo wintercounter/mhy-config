d3f86af099b14bf332e79fd88df0e92aa98e939e
Andy1210 <andy1210@gmail.com>
2018-11-28 11:19:44 +0100

    chore(svgx): width and height attributes

--- webpack/root/module/rules/35-svgx.js
+++ webpack/root/module/rules/35-svgx.js
@@ -3,7 +3,13 @@ module.exports.default = rules => [
     {
         test: /\.svgx$/,
         use: {
-            loader: require.resolve('svg-react-loader')
+            loader: require.resolve('svg-react-loader'),
+            options: {
+                attrs: {
+                    width: '1em',
+                    height: '1em'
+                },
+            }
         }
     }
 ]
